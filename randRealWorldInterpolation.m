51126579b5caa530dfe218f54338aae0953debd5
melton-jason <meltonjason12@yahoo.com>
2024-12-12 11:57:26 -0600

    Add images of real-world interpolants

--- randRealWorldInterpolation.m
+++ randRealWorldInterpolation.m
@@ -20,7 +20,7 @@ function a = RandScreenvBattery(random_samples)
     x = w(:,4); % screen on time
     y = w(:,5); % battery drain
     eval_inputs = linspace(min(x), max(x));
-    plot_methods(x, y, x, 'Google Pixel 5 Screen on Time v Battery');
+    plot_methods(x, y, x, 'Randomly Sampled Google Pixel 5 Screen on Time v Battery');
     disp('google pixel data:')
     disp(w)
 
@@ -29,7 +29,7 @@ function a = RandScreenvBattery(random_samples)
     x1 = w1(:,4);
     y1 = w1(:,5);
     eval_inputs1 = linspace(min(x1), max(x1));
-    plot_methods(x1, y1, x1, 'iPhone 12 Screen on Time v Battery');
+    plot_methods(x1, y1, x1, 'Randomly Sampled iPhone 12 Screen on Time v Battery');
     disp('Iphone data:' );
     disp(w1)
 
@@ -38,7 +38,7 @@ function a = RandScreenvBattery(random_samples)
     x2 = w2(:,4);
     y2 = w2(:,5);
     eval_inputs2 = linspace(min(x2), max(x2));
-    plot_methods(x2, y2, x2, 'One Plus 9 Screen on Time v Battery');
+    plot_methods(x2, y2, x2, 'Randomly Sampled One Plus 9 Screen on Time v Battery');
     disp('One Plus 9 data:' );
     disp(w2)
 
@@ -47,7 +47,7 @@ function a = RandScreenvBattery(random_samples)
     x3 = w3(:,4);
     y3 = w3(:,5);
     eval_inputs3 = linspace(min(x3), max(x3));
-    plot_methods(x3, y3, x3, 'Samsung Galaxy 21 Screen on Time v Battery');
+    plot_methods(x3, y3, x3, 'Randomly Sampled Samsung Galaxy 21 Screen on Time v Battery');
     disp('Samsung Galaxy data:' );
     disp(w3)
 
@@ -56,7 +56,7 @@ function a = RandScreenvBattery(random_samples)
     x4 = w4(:,4);
     y4 = w4(:,5);
     eval_inputs4 = linspace(min(x4), max(x4));
-    plot_methods(x4, y4, x4, 'Xiaomi Screen on Time v Battery');
+    plot_methods(x4, y4, x4, 'Randomly Sampled Xiaomi Screen on Time v Battery');
     disp('Xiaomi data:' );
     disp(w4)
 end
@@ -76,7 +76,7 @@ function b = RandAppUsagevBattery(random_samples)
     x = w(:,3); % app usage
     y = w(:,5); % battery drain 
     eval_inputs = linspace(min(x), max(x));
-    plot_methods(x, y, x, 'Google Pixel 5 App Usage Time v Battery');
+    plot_methods(x, y, x, 'Randomly Sampled Google Pixel 5 App Usage Time v Battery');
     disp('google pixel data:')
     disp(w)
 
@@ -85,7 +85,7 @@ function b = RandAppUsagevBattery(random_samples)
     x1 = w1(:,3);
     y1 = w1(:,5);
     eval_inputs1 = linspace(min(x1), max(x1));
-    plot_methods(x1, y1, x1, 'iPhone 12 App Usage Time v Battery');
+    plot_methods(x1, y1, x1, 'Randomly Sampled iPhone 12 App Usage Time v Battery');
     disp('Iphone data:' );
     disp(w1)
 
@@ -94,7 +94,7 @@ function b = RandAppUsagevBattery(random_samples)
     x2 = w2(:,3);
     y2 = w2(:,5);
     eval_inputs2 = linspace(min(x2), max(x2));
-    plot_methods(x2, y2, x2, 'One Plus 9 App Usage Time v Battery');
+    plot_methods(x2, y2, x2, 'Randomly Sampled One Plus 9 App Usage Time v Battery');
     disp('One Plus 9 data:' );
     disp(w2)
 
@@ -103,7 +103,7 @@ function b = RandAppUsagevBattery(random_samples)
     x3 = w3(:,3);
     y3 = w3(:,5);
     eval_inputs3 = linspace(min(x3), max(x3));
-    plot_methods(x3, y3, x3, 'Samsung Galaxy 21 App Usage Time v Battery');
+    plot_methods(x3, y3, x3, 'Randomly Sampled Samsung Galaxy 21 App Usage Time v Battery');
     disp('Samsung Galaxy data:' );
     disp(w3)
 
@@ -112,15 +112,15 @@ function b = RandAppUsagevBattery(random_samples)
     x4 = w4(:,3);
     y4 = w4(:,5);
     eval_inputs4 = linspace(min(x4), max(x4));
-    plot_methods(x4, y4, x4, 'Xiaomi App Usage Time v Battery');
+    plot_methods(x4, y4, x4, 'Randomly Sampled Xiaomi App Usage Time v Battery');
     disp('Xiaomi data:' );
     disp(w4)
 end
 
 % To somewhat alleviate high-sensitivity in the Vandermonde, Newton, 
 % and Lagrange interpolants, instead of taking the entire data set, 
-% randomly take 30 rows from the data set and use those instead 
-random_samples = 30;
+% randomly take 15 rows from the data set and use those instead 
+random_samples = 15;
 
 RandScreenvBattery(random_samples)
 RandAppUsagevBattery(random_samples)
